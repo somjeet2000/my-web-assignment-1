4ef017d61ffa18b2807668ac8b46b39d14cb0fd2
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-08-28 19:16:36 +0530

    Route 1: POST /api/v1/order/create_order

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,19 +1,28 @@
 const express = require('express');
+const Orders = require('../models/Orders');
 const router = express.Router();
 
 // Route 1: Create Order using POST: /api/v1/order/create_order.
-router.post('/create_order', (request, response) => {
+router.post('/create_order', async (request, response) => {
   try {
-    const {
-      userName,
-      orderID,
-      orderStatus,
-      costOfOrder,
-      actualCost,
-      netProfit,
-      feedback,
-      rating,
-    } = request.body;
+    const { userName, orderStatus, costOfOrder, actualCost, feedback, rating } =
+      request.body;
+
+    // Generate the random Order ID
+    let orderIDGenerate = Math.floor(100000 + Math.random() * 900000);
+
+    const orderCreate = await Orders.create({
+      userName: userName,
+      orderID: orderIDGenerate,
+      orderStatus: orderStatus,
+      costOfOrder: costOfOrder,
+      actualCost: actualCost,
+      netProfit: costOfOrder - actualCost,
+      feedback: feedback,
+      rating: rating,
+    });
+
+    response.status(200).json({ orderCreate });
   } catch (error) {
     return response
       .status(500)
